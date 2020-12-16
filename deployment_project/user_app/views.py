bcc0a9b9ca90d2b9bf8f486a54f4aec19adc4539
KS99 <karimsadiki1@MacBook-Pro-de-Karim.local>
2020-12-16 22:58:54 +0100

    user views

--- deployment_project/user_app/views.py
+++ deployment_project/user_app/views.py
@@ -1,26 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from .forms import User_form
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
-
-def login_view(request):
-
-
-    return render(request, "user_app/login.html", {})
 
 
 
 def signup_view(request):
 
-    return render(request, "user_app/signup.html", {})
+    form = User_form()
+
+    if request.method == "POST":
+        form = User_form(request.POST)
+
+        if form.is_valid():
+
+            form.save()
+            obj = get_object_or_404(User, username=request.POST["username"])
+            User.objects.create(user=obj)
+
+            return HttpResponseRedirect("../login")
+
+        return render(request, "users/signup.html", {"form":form})
+
+    return render(request, "user_app/signup.html", {"form": form})
 
 
 
 def profile_view(request):
 
-    return render(request, "user_app/login.html", {})
+    obj = User.objects.get(username=request.user.username)
+
+    return render(request, "users/profile.html", {"person":obj})
 
 def logout_view(request):
 
